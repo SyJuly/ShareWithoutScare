a7755c4fe5ad8fabd54fec9c6d4b4540d90cfc01
Juliane Fink <jule-fink@gmx.de>
2018-03-20 16:20:11 +0100

    Add friend data and create individual site

--- app/shareWithoutScare/urls.py
+++ app/shareWithoutScare/urls.py
@@ -6,5 +6,6 @@ from shareWithoutScare.models import Friend
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^friends/', ListView.as_view(queryset=Friend.objects.all().order_by("-name")[:25], template_name="friends.html"))
+    url(r'^friends/', ListView.as_view(queryset=Friend.objects.all().order_by("-name")[:25], template_name="friends.html")),
+    url(r'^friend/(?P<pk>\d+)$', DetailView.as_view(model=Friend, template_name='friend.html')),
 ]
